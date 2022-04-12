74fce37012074f2aba494d1cce7ab317cf95d548
Opalephisla <35504111+Opalephisla@users.noreply.github.com>
2022-04-12 19:39:21 +0200

    fixing slash commands

--- bot/main.py
+++ bot/main.py
@@ -1,16 +1,16 @@
-import discord
 import os
-#import pynacl
-#import dnspython
-import server
-from discord.ext import commands
 
-from discord import Client, Intents, Embed
+import discord
+from discord import Client, Embed, Intents
+from discord.ext import commands
 from discord_slash import SlashCommand, SlashContext
 
-bot = Client(intents=Intents.default())
-slash = SlashCommand(bot)
+# import pynacl
+# import dnspython
+import server
+
 bot = commands.Bot(command_prefix="!")
+
 
 @slash.slash(name="test")
 async def test(ctx: SlashContext):
@@ -18,13 +18,12 @@ async def test(ctx: SlashContext):
     await ctx.send(embed=embed)
 
 
-TOKEN = os.getenv("DISCORD_TOKEN")
-
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user.name}({bot.user.id})")
     # change bot status to "Studying WEB Development"
     await bot.change_presence(activity=discord.Game(name="Studying WEB Development"))
+
 
 @bot.command()
 async def ping(ctx):
@@ -33,22 +32,27 @@ async def ping(ctx):
 
 messagecounts = {}
 
+
 @bot.command()
 async def serverstats(ctx):
-    embed=discord.Embed(title=f"Statistiques du serveur {ctx.guild.name}")
+    embed = discord.Embed(title=f"Statistiques du serveur {ctx.guild.name}")
     embed.add_field(name="Nombre d'utilisateurs:", value=ctx.guild.member_count, inline=False)
     embed.add_field(name="Nombres de salons:", value=len(ctx.guild.channels), inline=False)
     embed.add_field(name="Nombre total de messages envoyés:", value=messagecounts[ctx.guild.id], inline=False)
     await ctx.send(embed=embed)
 
+
 @bot.command()
 async def userinfo(ctx, member: discord.Member):
-    embed=discord.Embed(title=f"Statistiques de {member.name}")
+    embed = discord.Embed(title=f"Statistiques de {member.name}")
     embed.add_field(name="Nombre de messages envoyés:", value=messagecounts[member.id], inline=False)
     await ctx.send(embed=embed)
 
+
 @bot.event
 async def on_message(message):
+
+    await bot.process_commands(message)
 
     if message.author.id == bot.user.id:
         return
@@ -59,8 +63,12 @@ async def on_message(message):
 
     if message.content.endswith("quoi"):
         await message.channel.send("feur")
-    
-    await bot.process_commands(message)
+
+
+bot = Client(intents=Intents.default())
+slash = SlashCommand(bot)
 
 server.server()
+
+TOKEN = os.getenv("DISCORD_TOKEN")
 bot.run(TOKEN)
