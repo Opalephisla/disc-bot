6c2dc08f30bdf1eba751f11b6fe3a49d2861ee22
Opalephisla <lilian.evano@gmail.com>
2022-04-12 14:09:29 +0200

    fixing method for purge

--- bot/main.py
+++ bot/main.py
@@ -24,18 +24,16 @@ async def on_message(message):
     if message.content.endswith("quoi"):
         await message.channel.send("feur")
 
-    # counts how many messages are sent consecutively by a user
+    # counts how many messages are sent consecutively by a user and deletes them if it reaches 5
     if message.author.id == bot.user.id:
         return
-        if message.author.id not in server.users:
-            server.users[message.author.id] = [message.author, 1]
-        else:
-            server.users[message.author.id][1] += 1
-        
-        # if user has sent more than 5 messages consecutively, delete them
-        if server.users[message.author.id][1] > 5:
-            message.author.delete()
-            await message.channel.send(f"{message.author.mention} has been deleted for spamming")
+    if message.author.id in server.user_messages:
+        server.user_messages[message.author.id] += 1
+    else:
+        server.user_messages[message.author.id] = 1
+    if server.user_messages[message.author.id] == 5:
+        await message.channel.purge(limit=5)
+        server.user_messages[message.author.id] = 0
         
 server.server()
 bot.run(TOKEN)
